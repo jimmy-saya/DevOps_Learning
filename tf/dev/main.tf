31440c336d369666972701d94e8773df22db0b53
unknown <jimmy.sayamoungkhoune@gmail;com>
2020-10-12 21:42:14 +0200

    New deployment code

--- tf/dev/main.tf
+++ tf/dev/main.tf
@@ -13,9 +13,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "nodejs" {
-    ami             = "ami-06fd8a495a537da8b"
-    instance_type   = "t2.micro"
-    tags            =  {
-        Name        = "NodeJS"
+    ami                     = "ami-06fd8a495a537da8b"
+    instance_type           = "t2.micro"
+    security_groups         = [aws_security_group.allow_http_from_alb.name]
+    tags                    =  {
+        Name                = "NodeJS"
     }
 }
