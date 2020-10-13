de5068e7270dbe2ee68a72532c81ad337be119ae
unknown <jimmy.sayamoungkhoune@gmail;com>
2020-10-13 12:53:20 +0200

    Add Listener to ALB

--- tf/dev/elb_and_target_group.tf
+++ tf/dev/elb_and_target_group.tf
@@ -20,3 +20,14 @@ resource "aws_lb_target_group_attachment" "node-web-app-target-group-attachement
   target_id        = aws_instance.nodejs.id
   port             = 80
 }
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.node-web-app-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.node-web-app-target-group.arn
+  }
+}
--- tf/dev/main.tf
+++ tf/dev/main.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "nodejs" {
     ami                     = "ami-06fd8a495a537da8b"
     instance_type           = "t2.micro"
     security_groups         = [aws_security_group.allow_http_from_alb.name]
+    key_name                = "ubuntu_desktop"
     tags                    =  {
         Name                = "NodeJS"
     }
