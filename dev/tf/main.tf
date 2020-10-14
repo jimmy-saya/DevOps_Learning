5d063b7b231d5142e0dd6ff5713cb14419d94c1d
Jimmy <jimmy.sayamoungkhoune@gmail;com>
2020-10-14 16:41:08 +0200

    Update packer + terraform

--- dev/tf/main.tf
+++ dev/tf/main.tf
@@ -13,11 +13,16 @@ provider "aws" {
 }
 
 resource "aws_instance" "nodejs" {
-    ami                     = "ami-06fd8a495a537da8b"
+    #ami                     = "ami-06fd8a495a537da8b"
+    ami                     = "ami-0bce4e4fa191ff9a0"
     instance_type           = "t2.micro"
     security_groups         = [aws_security_group.allow_http_from_alb.name]
     key_name                = "ubuntu_desktop"
     tags                    =  {
         Name                = "NodeJS"
     }
+    user_data = <<-EOF
+	#! /bin/bash
+    sudo docker run -d --name node-web-app-c -p 80:8080 -v node-web-volume:/usr/src/app jimmy/node-web-app
+    EOF
 }
