c10270c5b08479aa9764493f116e46d01bffd67d
Jimmy <jimmy.sayamoungkhoune@gmail;com>
2020-10-19 14:27:23 +0200

    Update with Lauch Template and Auto Scaling Group

---
+++ dev/tf/auto_scaling.tf
@@ -0,0 +1,32 @@
+data "template_file" "user_data" {
+  template = <<-EOF
+        #! /bin/bash
+        sudo docker run -d --name node-web-app-c -p 80:8080 -v node-web-volume:/usr/src/app jimmy/node-web-app
+    EOF
+}
+
+resource "aws_launch_template" "First_LT" {
+    name                        = "First_LT"
+    image_id                    = "ami-0f5b11915630c7f4e"
+    instance_type               = "t2.micro"
+    key_name                    = "ubuntu_desktop"
+    vpc_security_group_ids      = [aws_security_group.allow_http_from_alb.id]
+    user_data                   = base64encode(data.template_file.user_data.rendered)
+}
+
+resource "aws_autoscaling_group" "First_AG" {
+    name                            = "First_AG"
+    availability_zones              = ["eu-west-1a"]
+    desired_capacity                = 1   
+    min_size                        = 1
+    max_size                        = 1
+    health_check_grace_period       = 300
+    health_check_type               = "ELB"
+    target_group_arns               = [aws_lb_target_group.node-web-app-target-group.arn]
+    launch_template {
+      id        = aws_launch_template.First_LT.id
+      version   = "$Latest"
+    }
+
+  
+}
